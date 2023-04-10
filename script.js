a7a35773d44c58be08dc4bbd3c4747a3149ecabc
Isaiah <116988172+igvim@users.noreply.github.com>
2023-04-10 12:28:15 -0400

    linted

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const newBookForm = document.querySelector('form');
 const libraryState = (() => {
   const library = [];
   const container = document.querySelector('.book-container');
-  const createCard = (storedBook) => {
+  const createCard = storedBook => {
     const bookCard = document.createElement('div');
     bookCard.classList.add('card');
     bookCard.textContent = storedBook.info();
@@ -13,11 +13,9 @@ const libraryState = (() => {
     const newBookTitle = document.querySelector('#title');
     const newBookAuthor = document.querySelector('#author');
     const newBookPages = document.querySelector('#pages');
-    //const isBookRead = 
+    // const isBookRead = 
     const Book = (title, author, pages, read) => {
-      const info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages`
-      }
+      const info = () => `${title} by ${author}, ${pages} pages`
       return { title, author, pages, read, info }
     }
     const newBook = Book(
